eb235ade1b37c322f1a2fff5b2346b869d1c1b8d
ian-developer <ianperret@gmail.com>
2026-05-15 19:35:35 +0200

    added templates

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,12 @@ main = Blueprint("main", __name__)
 def home():
     name = request.args.get("name", "Developer")
     return render_template("index.html", name=name)
+
+@main.route("/form")
+def form():
+    return render_template("form.html")
+
+@main.route("/submit", methods=['POST'])
+def handle_submit():
+    name = request.form.get('username')
+    return f"<h1>Your name is {name}</h1>"
