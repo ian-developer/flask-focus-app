5fb061cdcf1c7e351a467d7c2f61b87804e330a3
ian-developer <ianperret@gmail.com>
2026-05-19 23:27:17 +0200

    error page

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,9 @@ def home():
 def form():
     return render_template("form.html")
 
-@main.route("/dashboard", methods=['GET'])
+@main.route("/dashboard", methods=['GET', 'POST'])
 def handle_submit():
-    name = request.args.get('username', '"Empty"')
+    name = request.args.get('username')
+    if not request.args.get('username'):
+        return render_template("error.html")
     return render_template("dashboard.html", name=name)
